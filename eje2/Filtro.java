1db4f39f77e9ff270b0583c82a5ebff6cc26c3ee
kekoeze <57164769+kekoeze@users.noreply.github.com>
2022-04-20 18:30:36 -0300

    Update Filtro.java

--- eje2/Filtro.java
+++ eje2/Filtro.java
@@ -9,7 +9,7 @@ public class Filtro {
     public static List<String[]> filtrar(InterfaceWhere<String[]> c, List<String[]> csvData) {
         List<String[]> results = new ArrayList<String[]>();
         for (int i = 0; i < csvData.size(); i++) {
-            if (c.equals(csvData.get(i))) {
+            if (c.execute(csvData.get(i))) {
                 results.add(csvData.get(i));
             }
 
