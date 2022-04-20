99b458f2a9075b30827e5c58340dce712e982b99
kekoeze <57164769+kekoeze@users.noreply.github.com>
2022-04-20 18:28:30 -0300

    Update Filtro.java

--- eje2/Filtro.java
+++ eje2/Filtro.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 
 public class Filtro {
 
-    public static List<String[]> filtrar(Predicate<String[]> c, List<String[]> csvData) {
+    public static List<String[]> filtrar(InterfaceWhere<String[]> c, List<String[]> csvData) {
         List<String[]> results = new ArrayList<String[]>();
         for (int i = 0; i < csvData.size(); i++) {
             if (c.equals(csvData.get(i))) {
